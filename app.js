2716017806a47728c7ba3e1cbaf8a521d4cf7fe6
LucasIbanez13 <lucaas00129@gmail.com>
2023-07-21 16:27:19 -0300

    fix:add form

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ app.use(express.static('public'))
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "home.html"))
 );
-app.get("/", (req, res) =>
+app.get("/register", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "register.html"))
 );
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "views", "register.html"))
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
 );
+
 
 app.listen(PORT, () =>
   console.log("Server running in http://localhost:" + PORT)
